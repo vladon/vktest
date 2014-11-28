6534de0813af17db06b097b00e8ea948a7af3d13
Vladislav Yaroslavlev <vladon@vladon.com>
2014-11-28 23:42:01 +0300

    .

--- vktest.dpr
+++ vktest.dpr
@@ -9,7 +9,8 @@ uses
   VkApi.Utils in '..\vkapi\VkApi.Utils.pas',
   VkApi.Authenticator in '..\vkapi\VkApi.Authenticator.pas',
   VkApi.Types in '..\vkapi\VkApi.Types.pas',
-  VkApi.Constants in '..\vkapi\VkApi.Constants.pas';
+  VkApi.Constants in '..\vkapi\VkApi.Constants.pas',
+  VkApi.Exception in '..\vkapi\VkApi.Exception.pas';
 
 {$R *.res}
 
